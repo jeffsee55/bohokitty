3d0e102839b29a0e2648dd72a805b5de0f51eba8
Jeff <jeffsee.55@gmail.com>
2014-12-20 12:02:07 -0500

    Again with the roles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 set :stage, :production
-server '104.236.87.166', user: 'deploy', roles: %w{web app}
+server '104.236.87.166', user: 'deploy'
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
