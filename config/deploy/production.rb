93f82f1443f249ce08255d425f6874dc6fc4f254
Jeff <jeffsee.55@gmail.com>
2014-12-30 11:37:14 -0500

    Removed web role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@ role :app, %w{deploy@104.236.78.42}
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '104.236.78.42', user: 'deploy', roles: %w{app web}
+server '104.236.78.42', user: 'deploy', roles: %w{app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
