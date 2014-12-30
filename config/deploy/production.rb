66a217e32eccc17c84ade838dce81e7bd5b90775
Jeff <jeffsee.55@gmail.com>
2014-12-29 20:31:17 -0500

    Reconfig

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@ role :db,  %w{deploy@example.com}
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '127.0.0.1', user: 'deploy', roles: %w{app}
+server '104.236.78.42', user: 'deploy', roles: %w{app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
