a03dceaf777e2026fdf7487557f1bb37ef3ee092
Jeff <jeffsee.55@gmail.com>
2014-12-30 14:29:55 -0500

    Instagram config

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 require 'instagram'
 
 Instagram.configure do |config|
-  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
-  config.access_token = ENV['INSTAGRAM_ACCESS_TOKEN']
+  config.client_id = 1344012740c84f6c9c2ed5f947b694a1
+  config.access_token = 44674676.1344012.f6d06fba2d7842a493781f28c346f5ff
 end
