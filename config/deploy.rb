25e0ee4569fba8a6383f5236463631c0708c5fe1
Jeff <jeffsee.55@gmail.com>
2014-12-30 22:32:12 -0500

    New dotenv task

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,29 @@ namespace :deploy do
     end
   end
 
+  namespace :dotenv do
+
+    desc "Generate the .env vars file"
+    task :setup do
+      on roles :app do
+        #https://github.com/capistrano/sshkit/blob/master/EXAMPLES.md#upload-a-file-from-a-stream
+        info "Uploading #{shared_path}/config/.env"
+        file = "dotenv"
+        smart_template file, ".env"
+      end
+    end
+    after "deploy:started", "deploy:dotenv:setup"
+
+    desc "Symlink the .rbenv-vars file into latest release"
+    task :symlink do
+      on roles :app do
+        execute :ln, '-nfs', "#{shared_path}/config/.env", "#{release_path}/.env"
+      end
+    end
+    before "deploy:updated", "deploy:dotenv:symlink"
+
+  end
+
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
